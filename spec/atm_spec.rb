360a5220f0b2d90c0b4d7ba4b706d4405c475d59
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-10-30 17:56:04 +0100

    Modifys withdraw to deduct correct amount

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -6,13 +6,13 @@ describe Atm do
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
   end
-end
+
   it 'has 1000$ on initialize' do
     expect(subject.funds).to eq 1000
   end
 
   it 'funds are reduced at withdraw' do
-    subject.withdraw 50
+    subject.withdraw(50, account)
     expect(subject.funds).to eq 950
   end
   it 'allow withdraw if account has enough balance.' do
