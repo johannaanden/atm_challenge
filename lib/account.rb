36adf187b298620fe7d56cacfa468675d2adf2ee
Jonathan <jon1969edwards@gmail.com>
2018-11-01 14:47:15 +0100

    creates method to check expiry date

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,15 @@
 class Account
     attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
 
+    STANDARD_VALIDITY_YRS = 5
+    
     def initialize
-        @pin_code = produce_pin_code 
+        @pin_code = produce_pin_code
+        @exp_date = set_expire_date
+    end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
     end
     
     def produce_pin_code
