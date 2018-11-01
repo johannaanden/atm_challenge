8880b1b76694130a966abaa0d6027d854cd621f5
Johanna Andén <johannaanden@Johannas-MacBook-Pro.local>
2018-11-01 14:23:04 +0100

    Creates method to check number of digits in pin code

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,11 @@
 class Account
     attr_accessor :pin_code, :balance, :account_status, :exp_date, :owner
+
+    def initialize
+        @pin_code = produce_pin_code 
+    end
+    
+    def produce_pin_code
+        rand(1000..9999)
+    end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,4 +4,10 @@ require 'date'
 
 describe Account do
     let(:owner) { instance_double('Account', pin_code: '1234', exp_date: '04/19', account_status: :active) }
+
+    it 'check length of a number' do
+        number = 1234
+        number_length = Math.log10(number).to_i + 1
+        expect(number_length).to eq 4
+    end
 end
